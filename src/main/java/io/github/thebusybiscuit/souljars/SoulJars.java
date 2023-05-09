31c420469698f203d6299267b8ac593fb47c33b7
test137e29b <badxnameless@gmail.com>
2023-05-09 22:14:20 +0100

    Warn instead of error

--- src/main/java/io/github/thebusybiscuit/souljars/SoulJars.java
+++ src/main/java/io/github/thebusybiscuit/souljars/SoulJars.java
@@ -58,7 +58,7 @@ public class SoulJars extends JavaPlugin implements Listener, SlimefunAddon {
                 EntityType type = EntityType.valueOf(mob);
                 registerSoul(type);
             } catch (Exception x) {
-                getLogger().log(Level.SEVERE, "{0}: Possibly invalid mob type: {1}", new Object[] { x.getClass().getSimpleName(), mob });
+                getLogger().log(Level.WARNING, "{0}: Possibly invalid mob type: {1}", new Object[] { x.getClass().getSimpleName(), mob });
             }
         }
 
